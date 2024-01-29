841f5f6205fd5e1a72ebc652caaa4403bcebf2a2
MochYusuf <yusufjabriko@gmail.com>
2024-01-29 21:22:03 +0700

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 
 
 app.use('/', (req, res) => {
